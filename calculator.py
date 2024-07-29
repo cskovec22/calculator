ac346ea8c97854e70511b24814531a5e9a4f30de
Nikita Vasin <cskovec22@yandex.ru>
2024-07-29 17:45:28 +0700

    Добавлены функции умножения и деления в калькулятор.

--- calculator.py
+++ calculator.py
@@ -29,7 +29,41 @@ class Calculator():
 
         return a - b
 
+    def mul(self, a: float, b: float) -> float:
+        """
+        Перемножить два числа.
+
+        Параметры:
+            a (float): Первое число.
+            b (float): Второе число.
+
+        Результат:
+            float: Результат умножения двух чисел.
+        """
+
+        return a * b
+
+    def div(self, a: float, b: float) -> float | None:
+        """
+        Поделить одно число на другое.
+
+        Параметры:
+            a (float): Первое число.
+            b (float): Второе число.
+
+        Результат:
+            res (float | None): Результат деления или None,
+                если произошло деление на ноль.
+        """
+
+        try:
+            res = a / b
+        except ZeroDivisionError:   
+            print("Деление на ноль!")
+            return None
+        return res
+
 
 if __name__ == "__main__":
     calculator = Calculator()
-    print(calculator.sub(1, 55))
+    print(calculator.div(-6, -0))
