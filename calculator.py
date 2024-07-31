76b3955d14d27c675b85d43396ab65b31efae903
Nikita Vasin <cskovec22@yandex.ru>
2024-07-31 15:58:44 +0700

    Добавлена операция смены знака, добавлена функция работы калькулятора.

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+import os
+
+
 class Calculator():
     """Калькулятор."""
 
@@ -52,18 +55,89 @@ class Calculator():
             b (float): Второе число.
 
         Результат:
-            res (float | None): Результат деления или None,
+            res (float | str): Результат деления или сообщение об ошибке,
                 если произошло деление на ноль.
         """
 
         try:
-            res = a / b
-        except ZeroDivisionError:   
-            print("Деление на ноль!")
-            return None
+            res: float = a / b
+        except ZeroDivisionError:
+            return "Деление на ноль!"
         return res
+
+    def change_sign(self, a: float) -> float:
+        """
+        Изменить знак числа.
+
+        Параметры:
+            a (float): Число.
+
+        Результат:
+            float: Число с измененным знаком.
+        """
+
+        return -a
+
+
+def cls() -> None:
+    """Очистить консоль."""
+
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def main(calc: Calculator) -> None:
+    """
+    Функция работы калькулятора.
+
+    Параметры:
+        calc (Calculator): Экземпляр калькулятора.
+    """
+
+    math_operations = {
+        "+": calc.add,
+        "-": calc.sub,
+        "*": calc.mul,
+        "/": calc.div,
+        "cs": calc.change_sign
+    }
+    operation: str | None = None
+    number: float | None = None
+    current_value: float | None = None
+
+    while True:
+        input_item = input(
+            "Введите число, операцию (+, -, *, /, cs) или 'q' для выхода: "
+        )
+        cls()
+
+        if input_item == "q":
+            break
+
+        if input_item in math_operations and current_value is not None:
+            operation = input_item
+            if operation == "cs" and current_value is not None:
+                current_value = math_operations[operation](current_value)
+            print(current_value)
+            continue
+
+        try:
+            number = float(input_item)
+        except ValueError:
+            print("Неправильный ввод числа!")
+            continue
+
+        if current_value is None or operation is None:
+            current_value = number
+        elif operation == "cs":
+            current_value = math_operations[operation](current_value)
+        else:
+            current_value = math_operations[operation](current_value, number)
+        operation = None
+
+        print(current_value)
 
 
 if __name__ == "__main__":
-    calculator = Calculator()
-    print(calculator.div(-6, -0))
+    cls()
+    calculator: Calculator = Calculator()
+    main(calculator)
